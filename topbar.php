11b2a0dc0eba99ff696368c1ce0d32ee7169baa4
suryamshetty <91060537+suryamshetty@users.noreply.github.com>
2023-04-29 08:07:54 +0530

    Add files via upload

---
+++ topbar.php
@@ -0,0 +1,24 @@
+
+  <nav class="main-header navbar navbar-expand navbar-dark navbar-dark ">
+
+    <ul class="navbar-nav">
+      <?php if(isset($_SESSION['login_id'])): ?>
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+    <?php endif; ?>
+      <li>
+        <a class="nav-link text-white"  href="./" role="button"> <large><b>File Management System</b></large></a>
+      </li>
+    </ul>
+
+    <ul class="navbar-nav ml-auto">
+     
+      <li class="nav-item">
+        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+          <i class="fas fa-expand-arrows-alt"></i>
+        </a>
+      </li>
+    </ul>
+  </nav>
+
